0f00d02905a28e84dab2493c5ac1c862993a29ba
AntoineVigneronIsisCastres <antoine.vigneron@etud.univ-jfc.fr>
2023-03-16 12:10:30 +0100

    16/03/2023 12:10

---
+++ back/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+let world = require("./world");
+const server = new ApolloServer({
+    typeDefs, resolvers,
+    context: async ({ req }) => ({
+        world: world
+    })
+});
+const app = express();
+app.use(express.static('public'));
+server.start().then(res => {
+    server.applyMiddleware({ app });
+    app.listen({ port: 4000 }, () =>
+        console.log(`🚀 Server ready at
+   http://localhost:4000${server.graphqlPath}`)
+    );
+})
